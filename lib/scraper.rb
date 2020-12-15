132b4f018e7e3dce41d0d1c62e290756e8aeb28f
teskew <teskinder1@gmail.com>
2020-12-14 20:11:21 -0600

    commit tree 10b011d0c24c8ff8446b803781f8f5722b79b45d

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,8 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
+#html = open("https://flatironschool.com/")
+  
 
-html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(open("http://flatironschool.com/"))
+doc.css(".site-header__hero__headline") 
